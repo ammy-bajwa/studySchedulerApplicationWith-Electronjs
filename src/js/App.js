c0e34ee8f21d7ffa54255ca545d1fce54c88ecf5
ammy203 <33599273+ammy203@users.noreply.github.com>
2018-04-22 13:34:52 +0500

    Finalising Subject Component

--- src/js/App.js
+++ src/js/App.js
@@ -6,12 +6,14 @@ import ShowAllSubjects from './components/ShowAllSubjects';
 import Subject from './components/Subject';
 
 
+let subjectObj = {"id":"19c2de01-d95b-4543-8199-8bd430cbe78e",subject:"Islamiat",subjectNote:"ejhaksdjhlaksdhlashdlahdslwrejhaksdjhlaksdhlashdlahdslwrejhaksdjhlaksdhlashdlahdslwrejhaksdjhlaksdhlashdlahdslwrejhaksdjhlaksdhlashdlahdslwrejhaksdjhlaksdhlashdlahdslwrejhaksdjhlaksdhlashdlahdslwr",subjectTimeToStudy:0,"date":"April 21st 2018, 11:37:59 pm"}
+
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Subject />        
+        <Subject subjectObj = {subjectObj}/>        
       </div>
     );
   }
--- src/js/components/Subject.js
+++ src/js/components/Subject.js
@@ -6,11 +6,15 @@ var timerInterval;
 class Subject extends React.Component {
     timerHandler = (value) => {
         if(value == 'start') {
-            let totalSeconds = 3600;
+            let totalSeconds = this.props.subjectObj.subjectTimeToStudy;
              timerInterval = window.setInterval(()=>{
                 --totalSeconds;
                 let totalMinutes = Math.ceil((totalSeconds / 60) );
-                document.getElementById("timer").textContent  = `${totalMinutes}m  ${totalSeconds}s`;
+                if(totalSeconds <= 0){
+                    return  document.getElementById("timer").textContent  = `You Have Successfully Completed The Subject Study Task`;
+                }
+                return document.getElementById("timer").textContent  = `${totalMinutes} Minutes Which Is  ${totalSeconds} Seconds Are Remaining`;
+             
             },1000);
         } else if(value == 'stop'){
             clearInterval(timerInterval);
@@ -26,20 +30,33 @@ class Subject extends React.Component {
             text-light
             text-center
             bg-dark p-3 m-3">Subject</h4>
-        </div>       
-        <progress value="0" max="10" id="progressBar"></progress>
-            <div className="border m-2 p-2">
-                <h4 className="text-center">Subject Is {}</h4>
-                <h4 className="text-center" id="counter">Timer {}</h4>
-                <div id="timerDiv" className="border border-primary">
-                    <p id="timer"></p>
-                </div>
-            <div id="timerBTN">
-                <button className="btn btn-primary m-2" onClick={()=>{this.timerHandler('start')}}>Start</button>              
-                <button className="btn btn-primary m-2" onClick={()=>{
-                    this.timerHandler('stop');
-                }}>Stop</button>
-            </div>
+        </div>   
+        <h4 className="text text-primary text-center">Subject Is {this.props.subjectObj.subject}</h4>
+        <div className="border m-2">
+        <div className="mt-5 p-2" id="subjectDivLeft">
+               
+        <h5 className="text text-secondary text-center pt-6">This Is Counter For Subject</h5>
+        <div>
+            <h5 id="timer" className="text-primary m-5 text-center">Here Will Be Your Subject Counter</h5>
+        </div>
+    <div id="timerBTN">
+        <button className="btn btn-primary m-2" onClick={()=>{this.timerHandler('start')}}>Start</button>              
+        <button className="btn btn-primary m-2" onClick={()=>{this.timerHandler('stop')}}>Stop</button>
+    </div>
+    </div>
+    <div className="mt-5 p-2" id="subjectDivRight">
+       
+    <h5 className="text text-secondary text-center pt-6">Here Is Your Note For Subject</h5>
+    <div className="card">
+        <div className="card-body text-center" id="subjectCardDiv">
+            {this.props.subjectObj.subjectNote}
+        </div>
+    </div>
+    </div>
+        </div>    
+          
+            <div id="subjectOkBTN">
+                <button className="btn btn-danger p-3">Ok</button>              
             </div>
         </div>
     );
